91d3ac113270b877e98603da2ea36446390609dc
ran2356@hotmail.com <ran2356@hotmail.com>
2011-11-12 13:13:21 -0700

    Ryan: Relocated files into Other folder

---
+++ Other/database.php
@@ -0,0 +1,16 @@
+<?php
+
+$active_group = "default";
+
+$db['default']['hostname'] = "localhost";
+$db['default']['username'] = "root";
+$db['default']['password'] = "";
+$db['default']['database'] = "menu429_db";
+$db['default']['dbdriver'] = "mysql";
+$db['default']['dbprefix'] = "";
+$db['default']['active_r'] = TRUE;
+$db['default']['pconnect'] = FALSE;
+$db['default']['db_debug'] = TRUE;
+
+
+?>
