4f500cdf0eed8634ef461000a4358fa50cec625a
ran2356@hotmail.com <ran2356@hotmail.com>
2011-11-12 13:11:47 -0700

    Ryan: Modified this file

--- menu429.php
+++ menu429.php
@@ -1,30 +1,34 @@
 <html>
 <head>
 	<title>Menu429</title>
+	<!-- <link rel="stylesheet" type="text/css" href="menu429.css"/> -->
 </head>
-<body id="homepage">
-	<link rel="stylesheet" type="text/css" href="menu429.css"/>
+<body>
 	<div id="container">
 		<div id="header">
 		Menu429
 		</div>
-		<div id="bodyout">
-			<div id="leftpane">
-			<?php
-				for ($i = 1; $i < 5; $i++)
-				echo "Hello World!" . "<br />";
-				$k = 5;
-				while($k > 0)
-				{
-					echo "cook ryan." . "<br />";
-					$k--;
-				}
-			?>
+		<div id="leftpane">
+		
+		<a href="create.php">Create Recipes</a>
+		<br /><a href="browse.php">Browse Recipes</a>
+		<br /><a href="video.php">Our video</a>
 
-			</div>
-			<div id="body">
-
-			</div>
+		</div>
+		<div id="body">
+			<p>Test Body</p>
+			<div id="title"></div>
+			<div id="creator">Creator: </div>
+			<div id="time">Time Estimate: </div>
+			<div id="difficulty">Difficulty: </div>
+			<div id="picture"></div>
+			<div id="ingredients">Ingredients: </div>
+			<div id="ethnicity">Origin: </div>
+			<div id="type">Type: </div>
+			<div id="steps">Steps: </div>
+		</div>
+		<div id="footer">
+			<p>Test Footer</p>
 		</div>
 	</div>
 </body>
