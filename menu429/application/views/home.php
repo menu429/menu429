e36c13340366ed20afce3b8298a0081c53b39136
Phu Nguyen <pdnguyen@colorado.edu>
2011-11-12 13:27:28 -0700

    codeigniter

---
+++ menu429/application/views/home.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+		<title>Home Page</title>
+		
+	</head>
+<body>
+
+	<div id="container" style="width:948px">
+
+		<div id="header" style="">
+			<h1 style="margin-bottom:0;margin-left:331px;">Menu429</h1>
+			<hr />
+		</div>
+
+		<div id="menu" style="width:331px;float:left;text-align: center;">
+			<?php
+				echo anchor('site/create', 'Create', 'title="Create"');
+			?><br />
+			<b>Browse</b><br />
+		</div>
+
+		<div id="content" style="width:617px;float:left;">
+			<?php
+				foreach ($records as $row) {
+					echo '<h2>'.$row->recipe_name.'</h2>';
+					echo '<h4><i>Created By: '.$row->creator_name.'</i></h4>';
+					echo $row->steps.'<hr />';
+				}
+			?>
+		</div>
+	</div>
+</body>
+</html>
