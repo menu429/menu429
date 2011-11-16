b9b809dccbc0ded075f66448040f9a763853f81d
Katie P <kaytee.pham@gmail.com>
2011-11-15 17:27:03 -0700

    Fixed create.php and added the two button graphics

--- application/views/create.php
+++ application/views/create.php
@@ -9,7 +9,7 @@
 			<?=form_open('site/insert');?>
 			<p>Title: <input type="text" name="recipe_name"></p>
 			<p>Description: <textarea name="recipe_description" rows="8" cols="40"></textarea></p>
-               <p>Time Estimated: <select name="time_estimate" id="time_est" multiple onchange="" size="1">
+               <p>Time Estimated: <select name="time_estimate" id="time_est" size="1">
                    <option value=".5">30 mins</option>
                    <option value=".75">45 mins</option>
                    <option value="1">1 Hour</option>
@@ -20,12 +20,12 @@
 			<p>Ingredients: <textarea name="ingredients" rows="8" cols="40"></textarea></p>
 			<p>Steps: <textarea name="steps" rows="8" cols="40"></textarea></p>
 			<p>Created By: <input type="text" name="creator_name"></p>
-               <p>Category: <select name="category_type" id="category" multiple onchange="" size="1">
+               <p>Category: <select name="category_type" id="category" size="1">
                    <option value="main">Main</option>
                    <option value="side">Side</option>
                    <option value="drinks">Drinks</option>
                </select>
-               <p>Difficulty: <select name="difficulty" id="difficulty" multiple onchange="" size="1">
+               <p>Difficulty: <select name="difficulty" id="difficulty" size="1">
                    <option value="Easy">Easy</option>
                    <option value="Medium">Medium</option>
                    <option value="Hard">Hard</option>
